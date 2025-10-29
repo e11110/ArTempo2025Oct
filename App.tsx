bbbc0eb5e77a3fed62619cc7c691f9022e40bb46
Emma Seymour <emma_seymour@icloud.com>
2025-10-29 21:29:19 +0800

    two area background

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.upperArea} />
+      <View style={styles.lowerArea} />
+      <StatusBar style="light" />
     </View>
   );
 }
@@ -13,8 +14,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: 'column',
+  },
+  upperArea: {
+    flex: 1,
+    backgroundColor: '#4A4A4A',
+  },
+  lowerArea: {
+    flex: 0.95,
+    backgroundColor: '#605A5A',
   },
 });
